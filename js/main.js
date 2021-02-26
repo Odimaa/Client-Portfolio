54ae3f1f250544e5468346db75329b6daa198e80
Odimaa <odimaa.horsfall@gmail.com>
2021-02-26 20:35:11 +0100

    Added my project

---
+++ js/main.js
@@ -0,0 +1,178 @@
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas__menu__wrapper").addClass("show__offcanvas__menu");
+        $(".offcanvas__menu__overlay").addClass("active");
+    });
+
+    $(".canvas__close, .offcanvas__menu__overlay").on('click', function () {
+        $(".offcanvas__menu__wrapper").removeClass("show__offcanvas__menu");
+        $(".offcanvas__menu__overlay").removeClass("active");
+    });
+
+    /*------------------
+        Accordin Active
+    --------------------*/
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().removeClass('active');
+    });
+
+    /*------------------
+        Navigation
+    --------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+
+
+
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    var hero_s = $(".hero__slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*------------------
+        Testimonial Slider
+    --------------------*/
+    $(".testimonial__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            320: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            }
+        }
+    });
+
+
+    // Porfolio isotope and filter
+    $(window).on('load', function () {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+
+        $('#portfolio-flters li').on('click', function () {
+            $("#portfolio-flters li").removeClass('filter-active');
+            $(this).addClass('filter-active');
+
+            portfolioIsotope.isotope({
+                filter: $(this).data('filter')
+            });
+            aos_init();
+        });
+
+        // Initiate venobox (lightbox feature used in portofilo)
+        $(document).ready(function () {
+            $('.venobox').venobox();
+        });
+    });
+
+    // Clients carousel (uses the Owl Carousel library)
+    $(".clients-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 4
+            },
+            900: {
+                items: 6
+            }
+        }
+    });
+
+    // Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+
+
+    /*------------------
+        Radio btn
+    --------------------*/
+    $(".pricing__swipe-btn label").on('click', function (e) {
+        $(".pricing__swipe-btn label").removeClass("active");
+        $(this).addClass("active");
+
+        if (e.target.htmlFor == 'month') {
+            $(".yearly__plans").removeClass('active');
+            $(".monthly__plans").addClass('active');
+        } else if (e.target.htmlFor == 'yearly') {
+            $(".monthly__plans").removeClass('active');
+            $(".yearly__plans").addClass('active');
+        }
+    });
+    /*------------------
+        Achieve Counter
+    --------------------*/
+    $('.achieve-counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+})(jQuery);
